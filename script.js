50697c1334daf47877bf64677ab0b98573c77527
Prince Mishra <23btcse027@shiats.edu.in>
2025-09-18 21:38:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scroll for all links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+    // Close mobile menu if open
+    document.getElementById("nav-toggle").checked = false;
+  });
+});
+
